3b9f1a8f818baa397eef39a23670e21971000ef1
Vitto95 <grimaldi.vitto@gmail.com>
2021-06-07 14:31:26 +0200

    impostazione logica

--- index.php
+++ index.php
@@ -7,12 +7,40 @@
   <title>Domande frequenti - Privacy e termini - Google</title>
 </head>
 <body>
+
+
    <?php 
    
-    $faqs
+    $faqs = [
+      [
+        "title" => "titolo1",
+        "content" => ["contenuto paragrafo 1.1", "contenuto paragrafo 1.2", "contenuto paragrafo 1.3"]
+      ], 
+      [
+        "title" => "titolo2",
+        "content" => ["contenuto paragrafo 2.1", "contenuto paragrafo 2.2", "contenuto paragrafo 2.3"]
+      ],
+      [
+        "title" => "titolo3",
+        "content" => ["contenuto paragrafo 3.1", "contenuto paragrafo 3.2", "contenuto paragrafo 3.3"]
+      ]
+    ];
    
    ?>
 
-  
+   <?php foreach($faqs as $faq){ ?>
+      <?php foreach($faq["content"] as $paragraph){ ?>
+
+   <h2> 
+    <?php echo $faq["title"] ?> 
+   </h2>
+
+   <p>
+   <?php echo $paragraph ?> 
+   </p>
+
+   <?php } ?>
+    <?php } ?>
+
 </body>
 </html>
